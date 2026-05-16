7def7e3a42371c78751043d834d4d20e1a2c2184
Zozopuzik <evgen.sabadash1337@gmail.com>
2026-05-16 21:51:53 +0300

    Initial Commit

---
+++ Kubarik/KubarikApp.swift
@@ -0,0 +1,17 @@
+//
+//  KubarikApp.swift
+//  Kubarik
+//
+//  Created by Yevhen Sabadash on 16.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct KubarikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
